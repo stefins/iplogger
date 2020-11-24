4b86d8f5c92fb1bb876d11a7d50ed0cd39d4551f
Stef <sstefin@bk.ru>
2020-11-24 01:19:45 -0500

    Adding shitty comments :)

--- utils.go
+++ utils.go
@@ -11,8 +11,10 @@ import (
 )
 
 func getIp() (Ip IpLine) {
+	// Function to get the current ip address of the device
 	resp, err := http.Get("https://ifconfig.me")
 	if err != nil {
+		// this returns 'Connection Not Available' if internet is not available
 		Ip = IpLine{"Connection Not Available", time.Now()}
 		return
 	}
@@ -23,6 +25,7 @@ func getIp() (Ip IpLine) {
 }
 
 func logToFile(content IpLine) {
+	// Function to log Ip Address to the destination file.
 	home, err := os.UserHomeDir()
 	ifFileDoesntExist(home, content)
 	f, err := os.OpenFile(home+"/.iplogger/log.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
@@ -32,6 +35,7 @@ func logToFile(content IpLine) {
 	defer f.Close()
 	lastIPLine := getLastLine()
 	if lastIPLine.Ip != content.Ip {
+		// Also logs the current time in RFC1123 format seperated by '*'
 		if _, err = f.WriteString(content.Ip + "*" + content.Time.Format(time.RFC1123) + "\n"); err != nil {
 			panic(err)
 		}
@@ -39,6 +43,7 @@ func logToFile(content IpLine) {
 }
 
 func readLineByLine() (ipline []IpLine) {
+	// Function to read all the IP address and time from the file
 	home, err := os.UserHomeDir()
 	file, err := os.Open(home + "/.iplogger/log.txt")
 	if err != nil {
@@ -49,6 +54,7 @@ func readLineByLine() (ipline []IpLine) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
+		// Spliting by '*'
 		dividedStrings := strings.Split(txt, "*")
 		parsedTime, err := time.Parse(time.RFC1123, dividedStrings[1])
 		if err != nil {
@@ -64,12 +70,15 @@ func readLineByLine() (ipline []IpLine) {
 }
 
 func getLastLine() (ipline IpLine) {
+	// Function to return the last line of the log file
 	lines := readLineByLine()
 	ipline = lines[len(lines)-1]
 	return
 }
 
 func ifFileDoesntExist(home string, content IpLine) {
+	// Function to create the file if it doesn't exist in the destination
+	// (also write the first line)
 	if !fileExists(home + "/.iplogger/log.txt") {
 		f, err := os.OpenFile(home+"/.iplogger/log.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 		if err != nil {
@@ -82,6 +91,7 @@ func ifFileDoesntExist(home string, content IpLine) {
 }
 
 func fileExists(name string) bool {
+	// Checking if a file exist or not!
 	if _, err := os.Stat(name); err != nil {
 		if os.IsNotExist(err) {
 			return false
